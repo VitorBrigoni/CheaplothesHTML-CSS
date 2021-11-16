531c5f4b02d31f7ff9410e7643e8004c21180599
Vitor Brigoni <vitor.240504@bento.local>
2021-11-16 09:35:02 -0300

    primeiro commit

---
+++ camisetas.php
@@ -0,0 +1,204 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body, html{
+        width:100%;
+        height: 100%;
+      }
+        h1{
+            color: lightgreen;
+        }
+        .logo{
+            width:80px;
+            height:80px;
+        }
+        #ativo{
+          color:lightgreen;
+        }
+        #ativo::after{
+          content:'';
+          display: block;
+          height:3px;
+          width:100%;
+          background-color: lightgreen;
+          position: absolute;
+          left: 0%;
+          transition: .5s;
+          opacity: 1;
+          transform: translate(0, 1.7em);
+        }
+        li{
+          position: relative;
+          font-size: 20px;
+          padding:20px;
+        }
+        .li::after{
+          content:'';
+          display: block;
+          height:3px;
+          width:100%;
+          background-color: lightgreen;
+          position: absolute;
+          left: 0%;
+          transition: .5s;
+          opacity: 0;
+        }
+        .nav-link:hover{
+          color:lightgreen;
+        }
+        .li:hover::after{
+          transform: translate(0, 1.7em);
+          width: 100%;
+          background-color: lightgreen;
+          left: 0%;
+          opacity: 1;
+        }
+        .col{
+          margin: 8px;
+          margin-bottom: 20px;
+        }
+        .btn{
+          background-color:green;
+          border-color: green;
+          color:white;
+          font-weight: bold;
+        }
+        .btn:hover{
+          transition: .5s;
+          background-color:lightgreen;
+          border-color: lightgreen;
+          color:white;
+        }
+        .carrinho{
+          margin-left:15px;
+        }
+    </style>
+    <title>Cheaplothes Camisetas</title>
+
+  </head>
+  <body>
+    <header class="p-3 mb-3 border-bottom">
+    <div class="container">
+      <div class="d-flex flex-wrap align-items-center justify-content justify-content-lg-start">
+        <a class="navbar-brand"><h1>CHEAPLOTHES</h1></a>
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="principal.php" class="nav-link px-2 link-secondary li">Home </a></li>
+          <li><a href="camisetas.php" id='ativo' class="nav-link px-2 link-secondary li"> Camisetas </a></li>
+          <li><a href="moletons.php"  class="nav-link px-2 link-dark li">Moletons</a></li>
+          <li><a href="bermudas.php" class="nav-link px-2 link-dark li">Bermudas</a></li>
+          <li><a href="sapatos.php" class="nav-link px-2 link-dark li">Sapatos</a></li>
+        </ul>
+
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+          <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+
+        <div class="dropdown text-end">
+          <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="user.png" alt="mdo" width="32" height="32" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
+            <li style='font-size: 15px; padding: 3px;'>>Usuário</li>
+            <li style='font-size: 15px; padding: 0px;'><hr class="dropdown-divider"></li>
+            <li style='font-size: 15px; padding: 3px;'><a class="dropdown-item" href="index.php">Sair</a></li>
+          </ul>
+        </div>
+        <div class='carrinho'>
+          <a href='carrinho.php'><img src="carrinho.png" alt="mdo" width="32" height="32" class=""></a>
+        </div>
+      </div>
+    </div>
+      </header>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+          <div class="col">
+          <div class="card" style="width: 18rem;">
+          <img src="camisetaExemplo.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">R$ 149,99</h5>
+            <p class="card-text">Camisa Nike Corinthians I 2021/22 Jogador Masculino</p>
+            <a href="#" class="btn btn-primary">Ampliar</a>
+          </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ carrinho.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Cheaplothes Carrinho</title>
+    <style>
+        * {
+            box-sizing: border-box;
+        }
+
+        html,
+        body {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            background-color: white;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .shopping-cart {
+            width: 750px;
+            margin: 80px auto;
+            background: #FFFFFF;
+            box-shadow: 2px 7px 47px 4px rgba(0, 0, 0, 0.10);
+            border-radius: 10px;
+            display: flex;
+            flex-direction: column;
+            padding: 30px;;
+        }
+
+        .title {
+            height: 60px;
+            border-bottom: 1px solid #E1E8EE;
+            padding: 20px 30px;
+            color: #5E6977;
+            font-size: 18px;
+            font-weight: 400;
+        }
+
+        .item {
+            padding: 20px 30px;
+            height: 120px;
+            display: flex;
+        }
+
+        .item:not(:first-child) {
+            border-top: 1px solid #E1E8EE;
+            border-bottom: 1px solid #E1E8EE;
+        }
+
+        .buttons {
+            position: relative;
+            padding-top: 30px;
+            margin-right: 60px;
+        }
+
+        .delete-btn {
+            display: inline-block;
+            Cursor: pointer;
+        }
+
+        .delete-btn {
+            width: 18px;
+            height: 17px;
+        }
+
+        .image {
+            width: 30%;
+            height: 100%;
+        }
+
+        .description {
+            padding-top: 10px;
+            margin-right: 60px;
+            width: 115px;
+        }
+
+        .description span {
+            display: block;
+            font-size: 14px;
+            color: #43484D;
+            font-weight: 400;
+        }
+
+        .description span:first-child {
+            margin-bottom: 5px;
+        }
+
+        .description span:last-child {
+            font-weight: 300;
+            margin-top: 8px;
+            color: #86939E;
+        }
+
+        .quantity {
+            padding-top: 20px;
+            margin-right: 60px;
+        }
+
+        .total-price {
+            display: flex;
+            align-items: center;
+        }
+
+        .quantity input {
+            -webkit-appearance: none;
+            border: none;
+            text-align: center;
+            width: 32px;
+            font-size: 16px;
+            color: #43484D;
+            font-weight: 300;
+        }
+
+        .plus-btn img {
+            margin-top: 2px;
+        }
+
+        button:focus,
+        input:focus {
+            outline: 0;
+        }
+        .numeroProd{
+            padding:5px;
+        }
+        .titulo{
+            color:lightgreen;
+            font-weight: bold;
+            font-size: 26px;
+        }
+        .buttao{
+          background-color:green;
+          border-color: green;
+          color:white;
+          font-weight: bold;
+          width:100px;
+          float:right;
+        }
+        .buttao:hover{
+          transition: .5s;
+          background-color:lightgreen;
+          border-color: lightgreen;
+          color:white;
+        }
+    </style>
+</head>
+
+<body>
+    <div class='shopping-cart'>
+        <header>
+            <span class='titulo'>Carrinho Cheaplothes</span>
+            <a href="principal.php" class="btn btn-primary buttao">Voltar</a>
+        </header>
+        <div class='title'>
+            Meus Pedidos
+        </div>
+        <span class='numeroProd'>Produto 1</span>
+        <div class='item'>
+            <div class='buttons'>
+                <img class='delete-btn' src='delete.png' alt=''>
+            </div>
+            <div class='image'>
+                <img src='sapatoExemplo.jpg' width='50%' height='100%' alt='' />
+            </div>
+            <div class='description'>
+                <span>Sapato Nike</span>
+                <span>Tamanho: 40</span>
+                <span>Branca</span>
+            </div>
+            <div class='quantity'>
+                <input value='1'>
+            </div>
+            <div class='total-price'>$149,99</div>
+        </div>
+        <span class='numeroProd'>Produto 2</span>
+        <div class='item'>
+            <div class='buttons'>
+                <img class='delete-btn' src='delete.png' alt=''>
+            </div>
+            <div class='image'>
+                <img src='camisetaExemplo.jpg' width='50%' height='100%' alt='' />
+            </div>
+            <div class='description'>
+                <span>Camiseta Time</span>
+                <span>Tamanho: 40</span>
+                <span>Branca</span>
+            </div>
+            <div class='quantity'>
+                <input value='1'>
+            </div>
+            <div class='total-price'>$149,99</div>
+        </div>
+        <span class='numeroProd'>Produto 3</span>
+        <div class='item'>
+            <div class='buttons'>
+                <img class='delete-btn' src='delete.png' alt=''>
+            </div>
+            <div class='image'>
+                <img src='camisetaExemplo.jpg' width='50%' height='100%' alt='' />
+            </div>
+            <div class='description'>
+                <span>Camiseta Time</span>
+                <span>Tamanho: 40</span>
+                <span>Branca</span>
+            </div>
+            <div class='quantity'>
+                <input value='1'>
+            </div>
+            <div class='total-price'>$149,99</div>
+        </div>
+        <span class='numeroProd'>Produto 4</span>
+        <div class='item'>
+            <div class='buttons'>
+                <img class='delete-btn' src='delete.png' alt=''>
+            </div>
+            <div class='image'>
+                <img src='camisetaExemplo.jpg' width='50%' height='100%' alt='' />
+            </div>
+            <div class='description'>
+                <span>Camiseta Time</span>
+                <span>Tamanho: 40</span>
+                <span>Branca</span>
+            </div>
+            <div class='quantity'>
+                <input value='1'>
+            </div>
+            <div class='total-price'>$149,99</div>
+        </div>
+        <span class='numeroProd'>Produto 5</span>
+        <div class='item'>
+            <div class='buttons'>
+                <img class='delete-btn' src='delete.png' alt=''>
+            </div>
+            <div class='image'>
+                <img src='camisetaExemplo.jpg' width='50%' height='100%' alt='' />
+            </div>
+            <div class='description'>
+                <span>Camiseta Time</span>
+                <span>Tamanho: 40</span>
+                <span>Branca</span>
+            </div>
+            <div class='quantity'>
+                <input value='1'>
+            </div>
+            <div class='total-price'>$149,99</div>
+        </div>
+    </div>
+</body>
+
+</html>
